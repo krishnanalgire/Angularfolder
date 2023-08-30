3d3758167cf6c693e2955a391032ad869f9ec767
krishna <krishnanalgire95@gmail.com>
2023-08-30 15:24:00 +0530

    compnentlist created

--- contact/src/app/contactlist/contactlist.component.ts
+++ contact/src/app/contactlist/contactlist.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ContactService } from '../contact.service';
+import { Contact } from '../contact/contact.component';
 
 @Component({
   selector: 'app-contactlist',
   templateUrl: './contactlist.component.html',
   styleUrls: ['./contactlist.component.css']
 })
-export class ContactlistComponent {
+export class ContactlistComponent implements OnInit {
 
+  contacts:Contact[]=[ ];
+  constructor(private contactService:ContactService, private router:Router) {
+  }
+
+  ngOnInit(): void {
+    this.getAllContacts();
+  }
+
+  getAllContacts(){
+    this.contactService.getAllContacts().subscribe(
+      data=>{
+        this.contacts=data;
+      }
+    );
+  }
+  //remove a contact
+  deleteContact(id:number){
+    this.contactService.removeContact(id).subscribe(
+      data=>{
+        console.log("SUCCESSFULL........");
+        console.log(data);
+        this.getAllContacts();
+      },
+      error=>{
+        console.log("FAILED........");
+        console.log(error);
+      }
+    )
+   }
+   //edit a contact
+  editContact(id:number){
+    console.log("Edited :: "+id);
+    this.router.navigate(['/edit',id]); 
+  }
 }
